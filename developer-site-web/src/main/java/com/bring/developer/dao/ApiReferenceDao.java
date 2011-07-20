fefd6d47602bfcde716b1f6c498ba342b97ccb24
Nicolaj Petersen <nicolajbroby@gmail.com>
2011-07-20 11:27:56 +0200

    Changed Parser

--- developer-site-web/src/main/java/com/bring/developer/dao/ApiReferenceDao.java
+++ developer-site-web/src/main/java/com/bring/developer/dao/ApiReferenceDao.java
@@ -1,19 +1,23 @@
 package com.bring.developer.dao;
 
 import java.io.InputStream;
-
 import javax.xml.bind.JAXBException;
-
 import com.bring.developer.response.Document;
 
 public class ApiReferenceDao {
         
+    private XmlParser<Document> xmlParser;
+    
+    public ApiReferenceDao(){
+        this.xmlParser = new XmlParser<Document>(Document.class);
+    }
+    
     public Document query(String api) throws JAXBException{
         return query(getClass().getResourceAsStream(api + ".xml"));
     }
     
     public Document query(InputStream inputStream) throws JAXBException{
         System.out.println(inputStream);
-        return new XmlParser().unmarshal(inputStream);
+        return xmlParser.unmarshal(inputStream);
     }
 }
