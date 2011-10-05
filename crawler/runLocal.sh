852710820c086cd13d8dd3efd7e19d97c015d20c
Christian Schwarz <christian.schwarz@bekk.no>
2011-10-05 15:05:55 +0200

    corrected url

--- crawler/runLocal.sh
+++ crawler/runLocal.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sed -e 's|xSERVERx|localhost:8082|g' -e 's|xFRAKTGUIDEx|localhost:8080|' morebot.xml.template > morebot.xml
+sed -e 's|xSERVERx|local.bring.no:8082|g' -e 's|xFRAKTGUIDEx|local.bring.no:8080|' morebot.xml.template > morebot.xml
 
 echo
 echo "Fraktguide Morebot Crawler test that looks for broken links and HTTP error codes when following links. Assuming localhost is up at 8080."
