e683c32147b79af7fa688d5712d883635c5bfa71
Tobias G. Waaler <tobiasgwaaler@gmail.com>
2012-07-17 09:33:37 +0200

    La inn html5shiv.js og disablet sticky sidebar for ie7

--- developer-site-web/src/main/webapp/js/stickysidebar.js
+++ developer-site-web/src/main/webapp/js/stickysidebar.js
@@ -2,21 +2,27 @@
  Sidebar
  --*/
 $(function () {
-//    If this page has a sidebar
-    if($('#sidebar').length != 0) {
-        var menudefault = $('#sidebar')[0].offsetTop;
-        alert(menudefault);
-
-        $(document).bind('ready scroll', function () {
-            var scroll = $(document).scrollTop();
-
-            if (scroll >= menudefault) {
-                $('#sidebar').addClass('stickymenu');
-            } else {
-                $('#sidebar').removeClass('stickymenu');
-            }
-
-        });
+    //if IE7
+    if ($.browser.msie && parseInt($.browser.version, 10) === 7) {
+        //Don't make the sidebar sticky
     }
+    //anything else than IE7
+    else {
+        //If this page has a sidebar
+        if ($('#sidebar').length != 0) {
+            var menudefault = $('#sidebar')[0].offsetTop;
+            alert(menudefault);
 
+            $(document).bind('ready scroll', function () {
+                var scroll = $(document).scrollTop();
+
+                if (scroll >= menudefault) {
+                    $('#sidebar').addClass('stickymenu');
+                } else {
+                    $('#sidebar').removeClass('stickymenu');
+                }
+
+            });
+        }
+    }
 });
