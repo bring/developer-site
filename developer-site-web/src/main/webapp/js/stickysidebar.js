73edeecfba95607027e4e7ce4087aaa7924bef2c
Tobias G. Waaler <tobiasgwaaler@gmail.com>
2012-07-17 08:17:30 +0200

    Modifiserte scriptet som lager sticky sidebar til å bare kjøre på side som har en sidebar

--- developer-site-web/src/main/webapp/js/stickysidebar.js
+++ developer-site-web/src/main/webapp/js/stickysidebar.js
@@ -1,18 +1,21 @@
 /*--
-    Sidebar
-    --*/
-$(function(){
-    var menudefault = $('#sidebar')[0].offsetTop;
+ Sidebar
+ --*/
+$(function () {
+//    If this page has a sidebar
+    if($('#sidebar').length != 0) {
+        var menudefault = $('#sidebar')[0].offsetTop;
 
-    $(document).bind('ready scroll',function(){
-        var scroll = $(document).scrollTop();
+        $(document).bind('ready scroll', function () {
+            var scroll = $(document).scrollTop();
 
-        if(scroll >= menudefault){
-            $('#sidebar').addClass('stickymenu');
-        } else {
-            $('#sidebar').removeClass('stickymenu');
-        }
+            if (scroll >= menudefault) {
+                $('#sidebar').addClass('stickymenu');
+            } else {
+                $('#sidebar').removeClass('stickymenu');
+            }
 
-    });
+        });
+    }
 
 });
