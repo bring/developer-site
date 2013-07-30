c25395c9136bceb73d4d3f65b10d5ae3b363ae97
Vidar Stendal <vidarstendal@gmail.com>
2013-07-30 09:57:52 +0200

    show errorcodes from PurchaseOrder api

--- developer-site-web/src/main/java/com/bring/developer/config/ApplicationConfig.java
+++ developer-site-web/src/main/java/com/bring/developer/config/ApplicationConfig.java
@@ -22,7 +22,7 @@ public class ApplicationConfig {
     public static String FRAKTGUIDE_URL_FOR_ENVIRONMENT = "http://fraktguide.bring.no/fraktguide"; //default
     public static String STATISTICS_TRACKER_ID_FOR_ENVIRONMENT = "1000918585199"; //default - test tracker
     public static String BOOKING_URL_FOR_ENVIRONMENT = "https://www.bring.no/booking/products"; //default
-    public static String PURCHASEORDER_URL_FOR_ENVIROMENT = "https://api.bring.com/po/api/"; //default
+    public static String PURCHASEORDER_URL_FOR_ENVIROMENT = "https://api.bring.com/po/api"; //default
     public static String GOOGLE_ANALYTICS_ID = "UA-33478893-1"; //default
     public static boolean CLASSPATH_XML_FILES = true; //default
 
