3ebcc2d206e85002f710c1b324a360acc5f118bb
Robin Skoglund <robin.skoglund@bring.com>
2015-11-11 09:02:28 +0100

    Sticky API menu
    Stickyness can be added to elements by setting the attribute
    `data-sticky="true"`. This will toggle a `stick` class on the element
    based on the pages's scroll position.

---
+++ js/sticky.js
@@ -0,0 +1,49 @@
+(function() {
+
+  var Sticky = function(element) {
+    this.element = element;
+    this.stickAtOffset = element.offsetTop;
+    this.stuck = false;
+
+    window.addEventListener('scroll', this.stickMeMaybe.bind(this));
+
+    this.stickMeMaybe();
+  }
+
+  Sticky.prototype.stickMeMaybe = function() {
+    var scrollPosition = window.pageYOffset;
+    var distance = this.element.offsetTop - scrollPosition;
+
+    if (!this.stuck && distance <= 0) {
+      console.log('Stick');
+      this.stick();
+    } else if (this.stuck && scrollPosition <= this.stickAtOffset) {
+      console.log('Unstick');
+      this.unstick();
+    } else {
+    }
+  }
+
+  Sticky.prototype.stick = function() {
+    this.element.classList.add('stick');
+    this.stuck = true;
+  }
+
+  Sticky.prototype.unstick = function() {
+    this.element.classList.remove('stick');
+    this.stuck = false;
+  }
+
+  var sticky = {
+    init: function() {
+      console.log('Initialize sticky elements');
+
+      var stickyElements = Array.prototype.slice.call(document.querySelectorAll('[data-sticky="true"]'));
+      stickyElements.forEach(function (element) {
+        new Sticky(element);
+      });
+    }
+  }
+
+  window.sticky = sticky;
+})();
