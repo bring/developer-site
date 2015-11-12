44f7e72be9fe0f97e7ccbdf2fc2ba6ae81943b00
Robin Skoglund <robin.skoglund@bring.com>
2015-11-12 13:29:08 +0100

    Add collapsible.js
    Collapsible is based on a `toggler` element and a `target` element.
    
        <a href="#" data-collapse="#target-element" class="collapsible__toggler--collapsed">Toggle an element</a>
        <div id="target-element" class="collapsible__item--collapsed"></div>
    
    The script will read the `data-collapse` attribute and add a click
    handler for the toggler element.
    
    The toggler will switch between the following classes for the toggler
    and the target element:
    
        collapsible__toggler--collapsed
        collapsible__toggler--expanded
        collapsible__item--collapsed
        collapsible__item--expanded
    
    That means the actual styling must be done with CSS.

---
+++ js/collapsible.js
@@ -0,0 +1,57 @@
+(function() {
+
+  var CLS_TOGGLER_COLLAPSED = 'collapsible__toggler--collapsed';
+  var CLS_TOGGLER_EXPANDED = 'collapsible__toggler--expanded';
+  var CLS_TARGET_COLLAPSED = 'collapsible__item--collapsed';
+  var CLS_TARGET_EXPANDED = 'collapsible__item--expanded';
+
+  var Collapsible = function(element) {
+    this.toggler = element;
+    this.target = document.querySelector(element.dataset.collapse);
+  };
+
+  Collapsible.prototype.toggle = function() {
+    var collapsed = this.toggler.classList.contains(CLS_TOGGLER_COLLAPSED) 
+                    || this.target.classList.contains(CLS_TARGET_COLLAPSED);
+
+    collapsed ? this.expand() : this.collapse();
+  };
+
+  Collapsible.prototype.collapse = function() {
+    this.toggler.classList.remove(CLS_TOGGLER_EXPANDED);
+    if (this.toggler.classList.contains(CLS_TOGGLER_COLLAPSED)) {
+      this.toggler.classList.add(CLS_TOGGLER_COLLAPSED);
+    }
+    
+    this.target.classList.remove(CLS_TARGET_EXPANDED);
+    if (this.target.classList.contains(CLS_TARGET_COLLAPSED)) {
+      this.target.classList.add(CLS_TARGET_COLLAPSED);
+    }
+  };
+
+  Collapsible.prototype.expand = function() {
+    this.toggler.classList.remove(CLS_TOGGLER_COLLAPSED);
+    if (this.toggler.classList.contains(CLS_TOGGLER_EXPANDED)) {
+      this.toggler.classList.add(CLS_TOGGLER_EXPANDED);
+    }
+    
+    this.target.classList.remove(CLS_TARGET_COLLAPSED);
+    if (this.target.classList.contains(CLS_TARGET_EXPANDED)) {
+      this.target.classList.add(CLS_TARGET_EXPANDED);
+    }
+  };
+
+  var collapsible = {
+    init: function() {
+      var togglers = Array.prototype.slice.call(document.querySelectorAll('[data-collapse]'));
+      togglers.forEach(function (element) {
+        element.addEventListener('click', function(e) {
+          e.preventDefault();
+          new Collapsible(this).toggle();
+        })
+      });
+    }
+  }
+
+  window.collapsible = collapsible;
+})();
