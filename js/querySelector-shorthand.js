ea3d885c3ecc3883cd5985b7f4be0c651f0a24c1
Robin Skoglund <robin.skoglund@bring.com>
2015-12-09 15:14:28 +0100

    Remove load-blocking queryselector javascript

--- js/querySelector-shorthand.js
+++
@@ -1,9 +0,0 @@
-function $ (selector, el) {
-     if (!el) {el = document;}
-     return el.querySelector(selector);
-}
-function $$ (selector, el) {
-     if (!el) {el = document;}
-     // return an array instead of a NodeList
-     return Array.prototype.slice.call(el.querySelectorAll(selector));
-}
