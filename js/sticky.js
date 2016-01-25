55a93a974cb0e430051e9748d2282d081e56086a
Robin Skoglund <robinsk@gmail.com>
2016-01-25 21:07:35 +0100

    Fix sticky position and width for sitemenu
    1. Add support for sticky--top and sticky--bottom
    2. Sticky-bottom must unfortunately set the CSS position in js
       (bootstrap also does this, so it should be ok).
    3. Hardcoded to use the height of 'body > footer' as offsetBottom
    4. Use the media query for desk, not 700px
    5. Use CSS3 calc function to set width correctly

--- js/sticky.js
+++ js/sticky.js
@@ -2,7 +2,8 @@
 
   var Sticky = function(element) {
     this.element = element;
-    this.stickAtOffset = element.offsetTop;
+    this.offsetTop = element.offsetTop;
+    this.offsetBottom = document.querySelector('body > footer').offsetHeight;
     this.stuck = false;
 
     window.addEventListener('scroll', this.stickMeMaybe.bind(this));
@@ -11,23 +12,41 @@
   }
 
   Sticky.prototype.stickMeMaybe = function() {
-    var scrollPosition = window.pageYOffset;
-    var distance = this.element.offsetTop - scrollPosition;
+    var scrollPosition        = window.pageYOffset;
+    var distance              = this.offsetTop - scrollPosition;
 
-    if (!this.stuck && distance <= 0) {
-      this.stick();
-    } else if (this.stuck && scrollPosition <= this.stickAtOffset) {
+    var documentHeight        = Math.max(
+      document.body.scrollHeight, document.documentElement.scrollHeight,
+      document.body.offsetHeight, document.documentElement.offsetHeight,
+      document.documentElement.clientHeight
+    );
+    var viewportHeight        = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    var maxScrollPosition     = documentHeight - viewportHeight;
+    var stickAtBottomPosition = maxScrollPosition - this.offsetBottom + (viewportHeight - this.element.offsetHeight);
+
+    if (scrollPosition >= stickAtBottomPosition) {
+      if (this.stuck != 'bottom') {
+        this.stick('bottom');
+      }
+      this.element.style.bottom = (this.offsetBottom - (maxScrollPosition - scrollPosition)) + 'px';
+    } else if (this.stuck != 'top' && distance <= 0) {
+      this.stick('top');
+      this.element.style.bottom = null;
+    } else if (this.stuck && scrollPosition <= this.offsetTop) {
       this.unstick();
     }
   }
 
-  Sticky.prototype.stick = function() {
-    this.element.classList.add('sticky');
-    this.stuck = true;
+  Sticky.prototype.stick = function(topOrBottom) {
+    this.element.classList.remove('sticky--top');
+    this.element.classList.remove('sticky--bottom');
+    this.element.classList.add('sticky--' + topOrBottom);
+    this.stuck = topOrBottom;
   }
 
   Sticky.prototype.unstick = function() {
-    this.element.classList.remove('sticky');
+    this.element.classList.remove('sticky--top');
+    this.element.classList.remove('sticky--bottom');
     this.stuck = false;
   }
 
