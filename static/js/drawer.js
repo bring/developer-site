b29a9b4254edc70d1a74d5c6e62850237bd51db9
Igor Jorganovic <igor.jorganovic@bring.com>
2022-01-20 09:31:46 +0100

    Rewrite the logic for left menu on smaller screens

--- static/js/drawer.js
+++ static/js/drawer.js
@@ -1,11 +1,38 @@
-const openDrawerMenu = () => {
+/*const openDrawerMenu = () => {
   const dataDrawer = document.querySelectorAll('[data-drawer]')
-  Array.prototype.forEach.call(dataDrawer, (drawerTargets) => {
+  dataDrawer.forEach(drawerTargets => {
     drawerTargets.classList.toggle('drawn')
   })
 }
 
 const drawerEvent = document.querySelectorAll('[data-drawer-event]')
-Array.prototype.forEach.call(drawerEvent, (drawerEventTargets) => {
-  drawerEventTargets.addEventListener('click', openDrawerMenu)
-})
+drawerEvent.forEach(drawerEventTargets => {
+    drawerEventTargets.addEventListener('click', openDrawerMenu)
+})*/
+
+const drawerBtn = document.querySelector('[data-drawer-btn]')
+
+if(drawerBtn) {
+  const drawers = document.querySelectorAll('[data-drawer]')
+  const drawerEventAreas = document.querySelectorAll('[data-drawer-event]')
+
+  const openDrawerMenu = () => {
+    drawers.forEach(drawer => {
+      drawer.classList.add('drawn')
+    })
+  }
+
+  const closeDrawerMenu = () => {
+    drawers.forEach(drawer => {
+      drawer.classList.remove('drawn')
+    })
+  }
+
+  drawerBtn.addEventListener('click', openDrawerMenu)
+
+  drawerEventAreas.forEach(drawerEvent => {
+    drawerEvent.addEventListener('click', function() {
+      closeDrawerMenu()
+    })
+  })
+}
