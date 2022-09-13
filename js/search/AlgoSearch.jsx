2c5e365e34a8318da72a589658e43fabab985f74
Arne Martin Aurlien <am@aurlien.net>
2022-09-13 09:08:31 +0200

    Get algolia api key from github secrets

--- js/search/AlgoSearch.jsx
+++ js/search/AlgoSearch.jsx
@@ -9,7 +9,7 @@ const searchClient = algoliasearch(
 )
 
 const AlgoSearch = () => (
-  <InstantSearch searchClient={searchClient} indexName={process.env.NODE_ENV == "production" ? "devsite_prod" : "devsite_qa"}>
+  <InstantSearch searchClient={searchClient} indexName={process.env.ALGOLIA_INDEX || "devsite_qa"}>
     <Result />
   </InstantSearch>
 )
