a8b0c96fd255c90d825f0ed1fcf5d221bfe371b6
Arne Martin Aurlien <am@aurlien.net>
2022-09-21 13:42:40 +0200

    And then we don't need to copy the manifest file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite"
 import legacy from "@vitejs/plugin-legacy"
 import react from "@vitejs/plugin-react"
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
   build: {
@@ -12,14 +11,5 @@ export default defineConfig({
       input: "./js/main.js",
     },
   },
-  plugins: [
-    legacy(), 
-    react(),
-    viteStaticCopy({
-      targets: [{
-        src: 'static/assets/manifest.json',
-        dest: '../../data'
-      }]
-    })
-  ],
+  plugins: [legacy(), react()],
 })
