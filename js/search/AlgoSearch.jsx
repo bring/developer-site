2bf14cf676fe1cd29daba8b5b65ce6534777575e
Arne Martin Aurlien <am@aurlien.net>
2022-09-27 09:41:54 +0200

    Don't hit algolia when the search is blank
    This was triggering a request to algolia on every page load, which we don't want.

--- js/search/AlgoSearch.jsx
+++ js/search/AlgoSearch.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
-import algoliasearch from 'algoliasearch/lite'
-import { InstantSearch } from 'react-instantsearch-dom'
-import Result from './Result.jsx'
+import React from "react"
+import algoliasearch from "algoliasearch/lite"
+import { InstantSearch } from "react-instantsearch-dom"
+import Result from "./Result.jsx"
 
-const searchClient = algoliasearch(
-  'PWK5X2R2GD',
-  '75cb7755dd0c05b3f3629e7a6793a2a3'
+const algoliaClient = algoliasearch(
+  "PWK5X2R2GD",
+  "75cb7755dd0c05b3f3629e7a6793a2a3"
 )
+
+const emptyAlgoliaResponse = () => ({
+  hits: [],
+  nbHits: 0,
+  nbPages: 0,
+  page: 0,
+  processingTimeMS: 0
+})
+
+const searchClient = {
+  ...algoliaClient,
+  search(requests) {
+    if (requests.every(({ params }) => !params.query)) {
+      return Promise.resolve({
+        results: requests.map(emptyAlgoliaResponse)
+      })
+    }
+
+    return algoliaClient.search(requests)
+  }
+}
 
 const AlgoSearch = () => (
   <InstantSearch searchClient={searchClient} indexName={__ALGOLIA_INDEX__}>
