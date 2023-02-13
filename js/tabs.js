44f84e3c571f24ede8612103ac3a23e620d93242
Halvor William Sanden <halvor.william.sanden@posten.no>
2023-02-13 13:36:30 +0100

    Add quotes to the data attribute selectors

--- js/tabs.js
+++ js/tabs.js
@@ -5,13 +5,13 @@ for (let tab of tabArray) {
     const panelId = tab.getAttribute('aria-controls')
     const panel = document.querySelector('#' + panelId)
 
-    const tabGroupArr = document.querySelectorAll('button[data-toggle=' + panel.dataset.tabGroup + ']')
+    const tabGroupArr = document.querySelectorAll('button[data-toggle="' + panel.dataset.tabGroup + '"]')
     for (let tab of tabGroupArr) {
       tab.classList.remove('mb-card--active')
       tab.setAttribute('aria-expanded', 'false')
     }
 
-    const tabPanelArr = document.querySelectorAll('div[data-tab-group=' + panel.dataset.tabGroup + ']')
+    const tabPanelArr = document.querySelectorAll('div[data-tab-group="' + panel.dataset.tabGroup + '"]')
     for (let tabPanel of tabPanelArr) {
       tabPanel.classList.add('dn')
     }
