8bc2f5a14c8d11e670c36ed8721e5450c5fb1a91
Arne Martin Aurlien <am@aurlien.net>
2023-05-25 13:43:53 +0200

    Format according to prettier rules

--- js/webcomponent/api-updates-subscribe/main.js
+++ js/webcomponent/api-updates-subscribe/main.js
@@ -1,139 +1,143 @@
-import apiUpdatesStyle from "./style.js";
-import apiUpdatesTemplate from "./template.js";
+import apiUpdatesStyle from "./style.js"
+import apiUpdatesTemplate from "./template.js"
 
 class ApiSubscribe extends HTMLElement {
   constructor() {
-    super();
+    super()
     //this.attachShadow({mode: "open"})
 
-    let modalAttr = this.attributes.modal;
-    let isModal = false;
+    let modalAttr = this.attributes.modal
+    let isModal = false
 
-    if(modalAttr) {
-      modalAttr = modalAttr.value;
-      if(modalAttr && modalAttr !== '' && modalAttr === 'true') {
-        isModal = true;
+    if (modalAttr) {
+      modalAttr = modalAttr.value
+      if (modalAttr && modalAttr !== "" && modalAttr === "true") {
+        isModal = true
       }
     }
 
-    this.render(isModal);
+    this.render(isModal)
 
-    const mcValidate = document.createElement("script");
-    mcValidate.type = "text/javascript";
-    mcValidate.async = true;
-    mcValidate.src = "//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js";
-    this.appendChild(mcValidate);
+    const mcValidate = document.createElement("script")
+    mcValidate.type = "text/javascript"
+    mcValidate.async = true
+    mcValidate.src =
+      "//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"
+    this.appendChild(mcValidate)
 
-    const mcValidateFn = document.createElement("script");
-    mcValidateFn.type = "text/javascript";
-    mcValidateFn.innerHTML = "(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';}(jQuery));var $mcj = jQuery.noConflict(true);";
+    const mcValidateFn = document.createElement("script")
+    mcValidateFn.type = "text/javascript"
+    mcValidateFn.innerHTML =
+      "(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';}(jQuery));var $mcj = jQuery.noConflict(true);"
     setTimeout(() => {
-      this.appendChild(mcValidateFn);
-    }, 300);
-
+      this.appendChild(mcValidateFn)
+    }, 300)
   }
 
   render(isModal) {
-    const style = apiUpdatesStyle(isModal);
-    const template = apiUpdatesTemplate(isModal);
+    const style = apiUpdatesStyle(isModal)
+    const template = apiUpdatesTemplate(isModal)
 
-    this.innerHTML = `${style} ${template}`;
+    this.innerHTML = `${style} ${template}`
 
-    const openModalBtn = document.querySelector('[data-api-updates-open]');
+    const openModalBtn = document.querySelector("[data-api-updates-open]")
 
-    const signupForm = document.getElementById('mc-embedded-subscribe-form');
-    const apiAreaRadios = signupForm.querySelectorAll('[data-api-area]');
-    const apiAreas = signupForm.querySelector('.api-area-checkboxes');
-    const apiAreaCheckboxes = apiAreas.querySelectorAll('input[type="checkbox"]');
-    const signupSubmitBtn = document.getElementById('mc-embedded-subscribe');
+    const signupForm = document.getElementById("mc-embedded-subscribe-form")
+    const apiAreaRadios = signupForm.querySelectorAll("[data-api-area]")
+    const apiAreas = signupForm.querySelector(".api-area-checkboxes")
+    const apiAreaCheckboxes = apiAreas.querySelectorAll(
+      'input[type="checkbox"]'
+    )
+    const signupSubmitBtn = document.getElementById("mc-embedded-subscribe")
 
     const validateCheckboxes = (e) => {
       // If second radio selected, check if at least one of the checkboxes are checked
-      let someSelected = false;
-      if(apiAreaRadios[1].checked) {
-        const errorMessage = document.getElementById('mce-error-response');
+      let someSelected = false
+      if (apiAreaRadios[1].checked) {
+        const errorMessage = document.getElementById("mce-error-response")
         apiAreaCheckboxes.forEach((checkbox) => {
-          if(checkbox.checked) {
-            someSelected = true;
+          if (checkbox.checked) {
+            someSelected = true
           }
-        });
-        if(!someSelected) {
-          errorMessage.innerText = '';
-          errorMessage.innerText = 'You must select at least one of the API areas.';
-          errorMessage.style.display = 'block';
-          e.preventDefault();
+        })
+        if (!someSelected) {
+          errorMessage.innerText = ""
+          errorMessage.innerText =
+            "You must select at least one of the API areas."
+          errorMessage.style.display = "block"
+          e.preventDefault()
         } else {
-          errorMessage.style.display = 'none';
-          errorMessage.innerText = '';
+          errorMessage.style.display = "none"
+          errorMessage.innerText = ""
         }
       }
     }
-  
+
     // Toggle visibility of checkboxes based on radio checked
     apiAreaRadios.forEach((radio) => {
-      radio.addEventListener('change', (e) => {
-          apiAreas.classList.toggle('hidden');
-          signupSubmitBtn.addEventListener('click', validateCheckboxes);
-          if(e.target.checked && e.target.id === 'mce-APIAREA-0') {
-            apiAreaCheckboxes.forEach((checkbox) => {
-              checkbox.checked = false;
-            });
-            signupSubmitBtn.removeEventListener('click', validateCheckboxes);
-          }
+      radio.addEventListener("change", (e) => {
+        apiAreas.classList.toggle("hidden")
+        signupSubmitBtn.addEventListener("click", validateCheckboxes)
+        if (e.target.checked && e.target.id === "mce-APIAREA-0") {
+          apiAreaCheckboxes.forEach((checkbox) => {
+            checkbox.checked = false
+          })
+          signupSubmitBtn.removeEventListener("click", validateCheckboxes)
+        }
       })
-    });
+    })
 
     // Hide the checkboxes again when the form is submitted
-    signupForm.addEventListener('submit', (e) => {
-      e.preventDefault();
+    signupForm.addEventListener("submit", (e) => {
+      e.preventDefault()
       setTimeout(() => {
-        const successMessage = document.getElementById('mce-success-response');
-        if(successMessage && successMessage.innerText.length > 0) {
-            apiAreas.classList.add('hidden');
-            setTimeout(() => {
-              successMessage.innerText = '';
-              successMessage.style.display = 'none';
-            }, 3000);
+        const successMessage = document.getElementById("mce-success-response")
+        if (successMessage && successMessage.innerText.length > 0) {
+          apiAreas.classList.add("hidden")
+          setTimeout(() => {
+            successMessage.innerText = ""
+            successMessage.style.display = "none"
+          }, 3000)
         }
-      }, 1000);
-    });
+      }, 1000)
+    })
 
-    if(isModal) {
-      if(openModalBtn) {
-        openModalBtn.addEventListener('click', () => {
-          open();
-        });
+    if (isModal) {
+      if (openModalBtn) {
+        openModalBtn.addEventListener("click", () => {
+          open()
+        })
       }
 
-      const closeModalAttr = document.querySelectorAll('[data-close-modal]');
+      const closeModalAttr = document.querySelectorAll("[data-close-modal]")
       closeModalAttr.forEach((closeModal) => {
-        closeModal.addEventListener('click', () => {
-          close();
-        });
-      });
+        closeModal.addEventListener("click", () => {
+          close()
+        })
+      })
 
-      this.addEventListener('keydown', (e) => {
+      this.addEventListener("keydown", (e) => {
         if (e.key === "Escape" || e.key === "Esc" || e.keyCode === 27) {
-          close();
+          close()
         }
-      });
+      })
     }
 
     const open = () => {
-      this.querySelector('.api-updates').classList.add('open');
-      const closeModalBtn = this.querySelector('[data-close-modal]');
-      if(closeModalBtn) {
-        closeModalBtn.focus();
+      this.querySelector(".api-updates").classList.add("open")
+      const closeModalBtn = this.querySelector("[data-close-modal]")
+      if (closeModalBtn) {
+        closeModalBtn.focus()
       }
     }
 
     const close = () => {
-      this.querySelector('.api-updates').classList.remove('open');
-      openModalBtn.focus();
+      this.querySelector(".api-updates").classList.remove("open")
+      openModalBtn.focus()
     }
   }
 }
 
 setTimeout(() => {
-  customElements.define("api-updates-subscribe", ApiSubscribe);
-}, 500);
+  customElements.define("api-updates-subscribe", ApiSubscribe)
+}, 500)
